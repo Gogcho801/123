ef74baeeda083c36701d0e49689ffff555d9e6d4
Gogcho801 <145712842+Gogcho801@users.noreply.github.com>
2023-09-21 12:36:24 -0700

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,101 @@
+from turtle import *
+
+
+#we want to paint a house
+
+#step 1: draw a square
+
+speed(30)
+width(7)
+color ("purple")
+forward(200)
+left(90)
+forward (200)
+left(90)
+forward (200)
+left(90)
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+color ("yellow")
+begin_fill()
+left(90)
+forward(120)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+color ("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+#draw windows
+color("blue")
+
+penup()
+goto(200,150)
+right(60)
+penup()
+forward(40)
+pendown()
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+
+
+
+penup()
+forward(100)
+pendown()
+
+color("blue")
+begin_fill()
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+end_fill()
+
+
+color("blue")
+begin_fill()
+penup()
+goto(200,150)
+forward(40)
+pendown()
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+end_fill()
+
+
+
+exitonclick()
